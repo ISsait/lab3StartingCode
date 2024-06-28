b40da35cf9bf2f54f8fce17e6e6c1dbbc099a75c
ISsait <152314143+ISsait@users.noreply.github.com>
2024-06-28 16:10:15 -0600

    initial commit

---
+++ src/utilities/DictionaryADT.java
@@ -0,0 +1,19 @@
+package utilities;
+
+import exceptions.DuplicateKeyException;
+
+/**
+* DictionaryADT.java
+*
+* @author your name
+* @version major.minor revision number starting at 1.0
+* 
+* Class Definition: This interface represents the public contract for the
+* implementation of Dictionary for the DictionaryADT Lab. 
+* @throws DuplicateKeyException
+*/
+
+public interface DictionaryADT<K,V>
+{
+    
+}
